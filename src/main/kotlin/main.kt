e09d5cd280e5036ba748999dd2499416bf7aab51
cubuspl42 <cubuspl42@gmail.com>
2022-01-25 11:08:24 +0100

    Move the test source file to `resources` directory

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,31 +2,11 @@ import bia.interpreter.Scope
 import bia.interpreter.evaluateBody
 import bia.parser.parseProgram
 
-const val source = """
-    val a = 10
-    val b = a + 20
-    val c = 15 * 2
-    val d = b == c
-    
-    def f(x) {
-        val a = x * 2
-        val b = a + 3
-        
-        def g(y) {
-            return a + b + y
-        }
-        
-        return g
-    }
-    
-    val h = f(1)
-    
-    return h(2)
-"""
-
 const val sourceName = "<main>"
 
 fun main() {
+    val source = getResourceAsText("test.bia") ?: throw RuntimeException("Couldn't load the source file")
+
     val programBody = parseProgram(
         sourceName = sourceName,
         source = source,
@@ -39,3 +19,6 @@ fun main() {
 
     println("Result: $result")
 }
+
+private fun getResourceAsText(path: String): String? =
+    object {}.javaClass.getResource(path)?.readText()
